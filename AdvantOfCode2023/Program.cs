a712f4b8f233631b9839a78ac50c8d2ec73db61b
Akash Buch <akash.buch@devitpl.com>
2023-12-07 16:03:43 +0530

    Day 7 part 1 done

--- AdvantOfCode2023/Program.cs
+++ AdvantOfCode2023/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using AdvantOfCode2023.Day6;
+using AdvantOfCode2023.Day7;
 
 Solutions day = new Solutions();
 day.First();
---
+++ AdvantOfCode2023/Utility.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvantOfCode2023
+{
+    public static class Utility
+    {
+        public static void PrintList(object list)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(list, Formatting.Indented));
+        }
+    }
+}
