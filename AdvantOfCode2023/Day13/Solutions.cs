c5f19ca3e9eab35043fab5301b84cef1a2e03304
Akash Buch <akash.buch@devitpl.com>
2023-12-16 16:24:56 +0530

    Change

--- AdvantOfCode2023/Day13/Solutions.cs
+++ AdvantOfCode2023/Day13/Solutions.cs
@@ -19,72 +19,123 @@ namespace AdvantOfCode2023.Day13
 
     public class Solutions
     {
-        public int LeftRight(int l , string left, int r , string right)
+        public string GetStringFromIndex(string[] str, int index)
         {
-            if (left.Equals(right)) return l + r + 2;
+            StringBuilder sb = new StringBuilder();
+            foreach (string s in str)
+            {
+                sb.Append(s[index]);
+            }
+            return sb.ToString();
+        }
+
+
+        public int GameCalculate(string str)
+        {
+            //top to down
+            var game = str.Split("\r\n");
+
+            // find mirror using x = x+1
+            var mirror_top = 0;
+            var mirror_bottom = 0;
+
+            for (int i = 1; i < game.Length - 1; i++)
+            {
+                if (game[i].Equals(game[i + 1]))
+                {
+                    mirror_top = i;
+                    mirror_bottom = i + 1;
+                    break;
+                }
+            }
+
+            // outward logic
+
+            var IsReflaction = false;
+            var refcount = 1;
+            while ((mirror_top - refcount) >= 0 && (mirror_bottom + refcount) < game.Count())
+            {
+                if (game[mirror_top - refcount].Equals(game[mirror_bottom + refcount]))
+                {
+                    if ((mirror_top - refcount) == 0 || (mirror_bottom + refcount) == game.Count() - 1)
+                    {
+                        IsReflaction = true;
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+                refcount++;
+            }
+
+            if (IsReflaction)
+            {
+                return (mirror_top + 1) * 100;
+            }
+
+
+
+
+            var mirror_left = 0;
+            var mirror_right = 0;
+
+            for (int i = 1; i < game[0].Length - 1; i++)
+            {
+                if (GetStringFromIndex(game,i).Equals(GetStringFromIndex(game, i + 1)))
+                {
+                    mirror_left = i;
+                    mirror_right = i + 1;
+                    break;
+                }
+            }
+
+
+            refcount = 1;
+            while ((mirror_left - refcount) >= 0 && (mirror_right + refcount) < game[0].Length)
+            {
+                if (GetStringFromIndex(game, mirror_left - refcount).Equals(GetStringFromIndex(game,mirror_right + refcount)))
+                {
+                    if ((mirror_left - refcount) == 0 || (mirror_right + refcount) == game[0].Length - 1)
+                    {
+                        IsReflaction = true;
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+                refcount++;
+            }
+
+
+            if (IsReflaction)
+            {
+                return (mirror_left + 1);
+            }
 
             return 0;
         }
 
-        public int GameCalculate(string str)
-        {
-            //left - right
-            var game = str.Split("\r\n");
-
-
-            var td_max = 0;
-            var td_max_count = 0;
-
-            for (int i = 0; i < game.Length - 1; i++)
-            {
-                var ans = LeftRight(i, game[i], i+1, game[i + 1]);
-
-                if(td_max_count < ans)
-                {
-                    td_max_count = ans;
-                    td_max = i;
-                }
-            }
-
-
-            var lr_max = 0;
-            var lr_max_count = 0;
-
-            for (int i = 0; i < game[0].Length - 1; i++)
-            {
-                var l = new string(game.Select(x => x[i]).ToArray());
-                var r = new string(game.Select(x => x[i+1]).ToArray());
-
-                var ans = LeftRight(i, l, i + 1, r);
-
-                if (lr_max_count < ans)
-                {
-                    lr_max_count = ans;
-                    lr_max = i;
-                }
-            }
-
-            Console.WriteLine($"top-down: {td_max} | left-right: {lr_max}");
-            return td_max > lr_max ? td_max : lr_max;
-        }
-
         public void First()
         {
-            var inputs = File.ReadAllText(@"C:\Users\akash.buch\source\repos\AdvantOfCode2023\AdvantOfCode2023\Day13\input13_test.txt");
+            var inputs = File.ReadAllText(@"C:\Users\akash.buch\source\repos\AdvantOfCode2023\AdvantOfCode2023\Day13\input13.txt");
 
             var games = inputs.Split("\r\n\r\n");
 
-            var t1 = GameCalculate(games[0]);
-            Console.WriteLine(t1);
+            var total = 0;
 
-            var t2 = GameCalculate(games[1]);
-            Console.WriteLine(t2);
+            int i = 1;
+            foreach (var game in games)
+            {
+                var t = GameCalculate(game);
+                total += t;
+                Console.WriteLine($"Count: {i++} | {t}");
+            }
 
-            //foreach (var game in games)
-            //{
-            //    var t = GameCalculate(game);
-            //    Console.WriteLine(t);
-            //}
+            Console.WriteLine(total);
         }
 
         public void Secound()
