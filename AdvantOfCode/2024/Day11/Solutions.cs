8482401cb6c7edee453bff5eb7735b127e3e2883
Akash Buch <akash.buch@devitpl.com>
2024-12-14 10:45:11 +0530

    C

--- AdvantOfCode/2024/Day11/Solutions.cs
+++ AdvantOfCode/2024/Day11/Solutions.cs
@@ -9,11 +9,11 @@ namespace AdvantOfCode._2024.Day11
 {
     public class Solutions
     {
-        static Dictionary<(List<long>, int), List<long>> memo = new Dictionary<(List<long>, int), List<long>>();
+        public Dictionary<long, List<long>> memo = new();
 
         private void Print(int i, List<long> change)
         {
-            Console.Write($"{i} = ");
+            Console.Write($"{i} | {change.Count} = ");
             foreach (var c in change)
             {
                 Console.Write($"{c} ");
@@ -26,71 +26,52 @@ namespace AdvantOfCode._2024.Day11
             var txt_inputs = File.ReadAllText(@"C:\Users\akash.buch\source\repos\AdvantOfCode2023\AdvantOfCode\2024\Day11\input.txt");
             var output = 0;
 
-            Dictionary<long, List<long>> cache= new();
 
             List<long> stones = txt_inputs.Split(' ').Select(x => Convert.ToInt64(x)).ToList();
+            
+            Dictionary<(long, int), List<long>> memo = new();
 
-            Dictionary<long, int> count= new();
-
-
-            // Initial setup
-            //List<long> stones = new List<long> { 125, 17 }; // Initial stones
-            int blinks = 75; // Number of blinks to simulate
-
-            Console.WriteLine("Initial stones: " + string.Join(" ", stones));
-
-            stones = TransformStonesRecursive(stones, blinks);
-            Console.WriteLine("Final stones after " + blinks + " blinks: " + stones.Count);
-
-        }
-
-        List<long> TransformStonesRecursive(List<long> stones, int blinks)
-        {
-            if (blinks == 0)
+            for (int i = 0; i < 75; i++)
             {
-                return stones;
-            }
+                List<long> new_stones = new();
 
-            if (memo.ContainsKey((stones, blinks)))
-            {
-                return memo[(stones, blinks)];
-            }
-
-            List<long> newStones = new List<long>();
-
-            foreach (var stone in stones)
-            {
-                if (stone == 0)
+                foreach (var stone in stones)
                 {
-                    // Rule 1: Replace 0 with 1
-                    newStones.Add(1);
+                    if (memo.ContainsKey((stone, i)))
+                    {
+                        new_stones.AddRange(memo[(stone, i)]);
+                    }
+                    else
+                    {
+                        if (stone == 0)
+                        {
+                            new_stones.Add(1);
+                            memo.Add((stone, i), new() { 1 });
+                        }
+                        else if (stone.ToString().Length % 2 == 0)
+                        {
+                            var s = SplitStone(stone);
+                            new_stones.AddRange(s);
+                            memo.Add((stone, i),new() { s[0], s[1] });
+                        }
+                        else
+                        {
+                            new_stones.Add(stone * 2024);
+                            memo.Add((stone, i), new() { stone * 2024 });
+                        }
+                    }
                 }
-                else if (HasEvenDigits(stone))
-                {
-                    // Rule 2: Split stone into two
-                    newStones.AddRange(SplitStone(stone));
-                }
-                else
-                {
-                    // Rule 3: Multiply by 2024
-                    newStones.Add(stone * 2024);
-                }
+
+                Console.WriteLine($"{i+1}\t{new_stones.Count}");
+
+                stones = new(new_stones);
             }
-
-            var result = TransformStonesRecursive(newStones, blinks - 1);
-            memo[(stones, blinks)] = result;
-
-            return result;
-        }
-        public bool HasEvenDigits(long number)
-        {
-            int digitCount = number.ToString().Length;
-            return digitCount % 2 == 0;
         }
 
         public List<long> SplitStone(long number)
         {
             string numStr = number.ToString();
+            
             int mid = numStr.Length / 2;
 
             long left = long.Parse(numStr.Substring(0, mid));
--- AdvantOfCode/Program.cs
+++ AdvantOfCode/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using AdvantOfCode._2024.Day12;
+using AdvantOfCode._2024.Day11;
 
 Solutions day = new Solutions();
 day.First();
